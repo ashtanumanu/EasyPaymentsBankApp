a209a7355092f3bd7565ce59086217a11b87f62d
tanishpandit <tanish.pandit2911@gmail.com>
2025-12-14 19:36:09 +0530

    added validation to request dto's.

---
+++ Accounts/Accounts/src/main/java/com/swissbank/accounts/constants/AccountConstants.java
@@ -0,0 +1,16 @@
+package com.swissbank.accounts.constants;
+
+public final class AccountConstants {
+
+    private AccountConstants() {}
+
+    public static final String SAVINGS = "SAVINGS";
+    public static final String ADDRESS = "123 Main St, Pune";
+    public static final String STATUS_CODE_201 = "201";
+    public static final String STATUS_MESSAGE_201 = "Account created successfully";
+    public static final String STATUS_CODE_200 = "200";
+    public static final String STATUS_MESSAGE_200 = "Request Proceed successfully";
+    public static final String STATUS_CODE_500 = "500";
+    public static final String STATUS_MESSAGE_500 = "an Error occured please try again later or contact developer team";
+
+}
